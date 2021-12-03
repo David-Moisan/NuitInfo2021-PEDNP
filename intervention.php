863c7e419607b5c75029e662ae7f70544d120a89
Clément GUITTENY <clement.guitteny@gmail.com>
2021-12-03 05:12:08 +0100

    Ajout debugs

--- intervention.php
+++ intervention.php
@@ -5,24 +5,25 @@ $dbport=3306;
 $dbuser="myadmin";
 $dbpasswd="Serveur123";
 
-echo "Connexion PDO";
 $pdo = new PDO('mysql:host='.$dbhost.';port='.$dbport.';dbname='.$db.'', $dbuser, $dbpasswd);
-echo "Passage en UTF8";
 $pdo->exec("SET CHARACTER SET utf8");
-echo "Prepare";
 $stmt = $pdo->prepare("SELECT IdOpe, NomOpe, BateauSauvetage, BateauSecouru, DateInte, Description, Lieu FROM Intervention i");
-echo "Execute";
 $stmt->execute();
-echo "Execute done";
 $rows = [];
 while ($row = $stmt->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)){
+    echo "rows";
     $rows[] = array_combine(['IdOpe','NomOpe', 'BateauSauvetage', 'BateauSecouru', 'DateOpe', 'Description','Lieu'], $row);
+    echo "stmt";
     $stmtequi = $pdo->prepare("SELECT DateNaissance, DateDeces, LieuNaissance, LieuDeces, Poste, Medailles FROM Personne p JOIN InterventionPersonne ip ON p.IdPersonne = ip.IdIntervention JOIN Itervention i ON i.IdOpe = ip.IdOpe WHERE IdOpe = ".$rows['IdOpe']);
+    echo "execute"
     $stmtequi->execute();
+    echo "done";
     $equipage = [];
+    echo "equipage";
     while ($rowequi = $stmtequi->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)){
         array_push($equipage,array_combine(['DateNaissance', 'DateDeces', 'LieuNaissance', 'LieuDeces', 'Poste', 'Medailles'], $rowequi));
     }
+    echo "endwhile"
     $rows['Equipage'] = $equipage;
 }
 echo json_encode($rows);
