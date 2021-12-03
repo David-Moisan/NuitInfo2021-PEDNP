83240a0eb34d4008b833ab045445421043a2ce3d
Clément GUITTENY <clement.guitteny@gmail.com>
2021-12-03 05:03:34 +0100

    Ajout des debugs

--- intervention.php
+++ intervention.php
@@ -5,10 +5,15 @@ $dbport=3306;
 $dbuser="myadmin";
 $dbpasswd="Serveur123";
 
+echo "Connexion PDO";
 $pdo = new PDO('mysql:host='.$dbhost.';port='.$dbport.';dbname='.$db.'', $dbuser, $dbpasswd);
+echo "Passage en UTF8";
 $pdo->exec("SET CHARACTER SET utf8");
+echo "Prepare";
 $stmt = $pdo->prepare("SELECT IdOpe, NomOpe, BateauSauvetage, BateauSecouru, DateOpe, Description, Lieu FROM Intervention i");
+echo "Execute";
 $stmt->execute();
+echo "Execute done";
 $rows = [];
 while ($row = $stmt->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)){
     $rows[] = array_combine(['IdOpe','NomOpe', 'BateauSauvetage', 'BateauSecouru', 'DateOpe', 'Description','Lieu'], $row);
